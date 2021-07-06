a49a23e3afc856f30497724eee3612fb42ad06ce
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-07-06 20:48:00 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import AllIssuesPage from './components/AllIssuesPage';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import AllIssuesPage from './pages/AllIssuesPage'
+import About from './pages/About'
+import Navbar from './components/Navbar'
 
 const App = () => {
-  return <AllIssuesPage />;
-};
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={AllIssuesPage} />
+        <Route exact path='/issues' component={AllIssuesPage} />
+        <Route exact path='/about' component={About} />
+      </Switch>
+    </Router>
+  )
+}
 
-export default App;
+export default App
